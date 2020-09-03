66a516dfb458fd1862aae88e5ecb4b3d609df195
Romano <romano.asciutto@ah.nl>
2020-09-03 15:47:05 +0200

    Check the amount of correct answers and print it with a Toast

--- app/src/main/java/nl/hva/madlevel1task2/MainActivity.kt
+++ app/src/main/java/nl/hva/madlevel1task2/MainActivity.kt
@@ -2,10 +2,46 @@ package nl.hva.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import nl.hva.madlevel1task2.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var correctAnswers: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+    }
+
+    /**
+     * Setup the onClick listener for the submit button
+     */
+    private fun initListeners() {
+        binding.submitButton.setOnClickListener { checkCorrectAnswers() }
+    }
+
+    /**
+     * Check the amount of correct answers and store it in the correctAnswers variable
+     */
+    private fun checkCorrectAnswers() {
+        if (binding.answer1.text.toString().toLowerCase(Locale.getDefault()) == "t") correctAnswers++
+        if (binding.answer2.text.toString().toLowerCase(Locale.getDefault()) == "f") correctAnswers++
+        if (binding.answer3.text.toString().toLowerCase(Locale.getDefault()) == "f") correctAnswers++
+        if (binding.answer4.text.toString().toLowerCase(Locale.getDefault()) == "f") correctAnswers++
+
+        showCorrectAnswers()
+        correctAnswers = 0
+    }
+
+    /**
+     * Print toast with the amount of correct answers
+     */
+    private fun showCorrectAnswers() {
+        Toast.makeText(this, getString(R.string.correct_answers, correctAnswers), Toast.LENGTH_SHORT).show()
     }
 }
